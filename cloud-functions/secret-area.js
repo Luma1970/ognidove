d411fc70ddbb73f997c37c8ea490da718bd3078d
Luma <luma@marinellaluft.com>
2020-04-16 15:44:33 +0200

    Aggiornata la funzione cloud per verificare le pwd

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,13 @@
-exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: "Benvenuti nell'area segreta"
-    })
-}
+exports.handler = function (event, context, callback) {
+const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    Il cielo è <strong>blu</strong> e 2 più 2 fa 4
+`
+    let body
+
+    event.body ? body = JSON.parse(event.body) : body = {} 
+
+    body.password == "javascript" ? callback(null, { statusCode: 200, body: secretContent }) : callback(null, {
+        statusCode: 401})
+
+    }
